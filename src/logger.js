5e94d9c83aca25d4fb283cd31a71f81aada75a17
Pablo Pereira <pablopereira27@gmail.com>
2026-04-19 20:22:01 -0300

    feat(logger): disable logger during tests

--- src/logger.js
+++ src/logger.js
@@ -3,7 +3,10 @@ const pino = require('pino');
 const isDev = process.env.NODE_ENV !== 'production';
 
 const logger = pino({
-    level: process.env.LOG_LEVEL || 'info',
+    level:
+        process.env.NODE_ENV === 'test'
+            ? 'silent'
+            : process.env.LOG_LEVEL || 'info',
     transport: isDev
         ? {
               target: 'pino-pretty',

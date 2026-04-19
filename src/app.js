ff7c77214369daa7749a5d359a614b079c5dc4d3
Pablo Pereira <pablopereira27@gmail.com>
2026-04-19 20:21:05 -0300

    refactor(structure): move application logic from index.js to app.js

---
+++ src/app.js
@@ -0,0 +1,32 @@
+// Core libraries
+const express = require('express');
+const pino = require('pino-http');
+
+// Utilitários e Middlewares
+const { swaggerUi, swaggerSpec } = require('./swagger/swagger-config');
+const logger = require('./logger');
+const errorHandler = require('./middlewares/error-handler.middleware');
+const routes = require('./routes');
+
+const app = express();
+
+// Middlewares Globais
+app.use(pino({ logger }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Rotas e Documentação
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use('/products', routes);
+
+// Health check
+app.get('/', (req, res) => {
+    res.send(
+        `${new Date().toLocaleString()} - Servidor em pleno funcionamento!`
+    );
+});
+
+// Middleware de erro
+app.use(errorHandler);
+
+module.exports = app;
--- src/index.js
+++ src/index.js
@@ -1,41 +1,11 @@
 // Configurações
 require('dotenv').config();
 const { AppDataSource } = require('./data-source');
-
-// Core libraries
-const express = require('express');
-const pino = require('pino-http');
-
-// Utilitários e Middlewares
-const { swaggerUi, swaggerSpec } = require('./swagger/swagger-config');
+const app = require('./app');
 const logger = require('./logger');
-const errorHandler = require('./middlewares/error-handler.middleware');
 const registerProcessHandlers = require('./utils/process-handlers');
 
-// Rotas
-const routes = require('./routes');
-
-const app = express();
 const port = process.env.APP_PORT || 3000;
-
-// Middlewares Globais
-app.use(pino({ logger }));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// Rotas e Documentação
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-app.use('/products', routes);
-
-// Health check
-app.get('/', (req, res) => {
-    res.send(
-        `${new Date().toLocaleString()} - Servidor em pleno funcionamento!`
-    );
-});
-
-// Middleware de erro
-app.use(errorHandler);
 
 // Captura de erros globais fora do ciclo de requisição
 registerProcessHandlers();
